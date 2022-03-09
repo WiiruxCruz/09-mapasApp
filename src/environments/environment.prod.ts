7b10f6a7577bec3cd28b7591ccc063c8cc1d3c01
WiiruxCruz <hialf.92@gmail.com>
2022-03-09 00:24:04 -0600

    Configuración de mapbox en el proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  mapboxToken: 'pk.eyJ1Ijoid2lpcnV4Y3J1eiIsImEiOiJjbDBodDl4angwYnV1M2VwbmU1aG5hMHRuIn0.6a1NnIH8VanJrnvcChC6JA'
 };
