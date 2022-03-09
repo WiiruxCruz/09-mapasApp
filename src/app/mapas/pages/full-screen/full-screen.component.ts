a1e6452a6b24badfc5bfec5c714f813dc8ea7b4a
WiiruxCruz <hialf.92@gmail.com>
2022-03-09 00:29:07 -0600

    Mostrar mapa en pantalla completa

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+import { environment } from '../../../../environments/environment.prod';
 
 @Component({
-  selector: 'app-full-screen',
-  templateUrl: './full-screen.component.html',
-  styles: [
-  ]
+	selector: 'app-full-screen',
+	templateUrl: './full-screen.component.html',
+	styles: [`
+		#mapa {
+			height: 100%;
+			width: 100%;
+		}
+	`]
 })
 export class FullScreenComponent implements OnInit {
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		//(mapboxgl as any).accessToken = 'pk.eyJ1Ijoid2lpcnV4Y3J1eiIsImEiOiJjbDBodDhjbTkwN3BmM2Vxd3ZuY21reG85In0.J6npKCfkCbG86unIOUzWaQ';
+		(mapboxgl as any).accessToken = environment.mapboxToken;
+
+		var map = new mapboxgl.Map({
+			container: 'mapa',
+			style: 'mapbox://styles/mapbox/streets-v11'
+		});
+	}
 
 }
