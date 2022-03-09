2ea812e9310d8da97596cf0967f3abc4346cb823
WiiruxCruz <hialf.92@gmail.com>
2022-03-09 00:36:48 -0600

    Punto central, zoom y accessToken

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -17,12 +17,16 @@ export class FullScreenComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit(): void {
+
+		console.log('FullScreen');
+
 		//(mapboxgl as any).accessToken = 'pk.eyJ1Ijoid2lpcnV4Y3J1eiIsImEiOiJjbDBodDhjbTkwN3BmM2Vxd3ZuY21reG85In0.J6npKCfkCbG86unIOUzWaQ';
-		(mapboxgl as any).accessToken = environment.mapboxToken;
 
 		var map = new mapboxgl.Map({
 			container: 'mapa',
-			style: 'mapbox://styles/mapbox/streets-v11'
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [ -96.1058807755658, 19.140431258623337 ],
+			zoom: 17
 		});
 	}
 
