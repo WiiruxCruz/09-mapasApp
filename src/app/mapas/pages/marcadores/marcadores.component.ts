725165c1ebacc44691047bfefc7f88589465cf4b
WiiruxCruz <hialf.92@gmail.com>
2022-03-09 23:49:00 -0600

    Agregar marcadores en el mapa

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
-  selector: 'app-marcadores',
-  templateUrl: './marcadores.component.html',
-  styles: [
-  ]
+	selector: 'app-marcadores',
+	templateUrl: './marcadores.component.html',
+	styles: [`
+	.mapa-container {
+		height: 100%;
+		width: 100%;
+	}
+	`]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+	@ViewChild('mapa') divMapa!: ElementRef;
 
-  constructor() { }
+	mapa!: mapboxgl.Map;
+	zoomLevel: number = 15;
+	center: [ number, number ] = [  -96.1058807755658, 19.140431258623337 ];
 
-  ngOnInit(): void {
-  }
+	constructor() { }
+	ngAfterViewInit(): void {
+		console.log('afterViewInit', this.divMapa);
 
+		this.mapa = new mapboxgl.Map({
+			container: this.divMapa.nativeElement,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: this.center,
+			zoom: this.zoomLevel
+		});
+
+		/*
+		Agregar marcador de texto
+		const markerHtml: HTMLElement = document.createElement('div');
+		markerHtml.innerHTML = 'Hola Mundo';
+
+		new mapboxgl.Marker({
+			element: markerHtml
+		})
+		*/
+
+		new mapboxgl.Marker()
+			.setLngLat( this.center )
+			.addTo( this.mapa );
+	}
 }
